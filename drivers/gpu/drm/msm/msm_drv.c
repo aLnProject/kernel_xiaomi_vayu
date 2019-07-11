bd8351860f37976227becd2dfa6b7e65cffdc4a8
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:53:59 +0900

    drm/msm: Use kmem_cache pool for struct vblank_work
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -64,6 +64,7 @@
 
 atomic_t resume_pending;
 wait_queue_head_t resume_wait_q;
+static struct kmem_cache *kmem_vblank_work_pool;
 
 static struct kmem_cache *kmem_vblank_work_pool;
 static DEFINE_MUTEX(msm_release_lock);
