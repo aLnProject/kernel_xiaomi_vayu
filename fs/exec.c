5f3bf8b64ec0299a18ba2ca124c7013c37db7610
John Galt <johngaltfirstrun@gmail.com>
2021-02-13 19:42:07 -0500

    kernel: affine surfaceflinger to big cluster
    oneplus did something similar in sched that was killed. There are often migrations occurring upon
    interactions.
    
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -78,6 +78,7 @@ static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
+#define SFLINGER_BIN_PREFIX "/system/bin/surfaceflinger"
 
 void __register_binfmt(struct linux_binfmt * fmt, int insert)
 {
@@ -1818,6 +1819,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 		if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		}
+		else if (unlikely(!strncmp(filename->name,
+					   SFLINGER_BIN_PREFIX,
+					   strlen(SFLINGER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
