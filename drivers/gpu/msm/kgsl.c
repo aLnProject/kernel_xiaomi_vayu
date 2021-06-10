2fcb5665a549bb890ea3459909d7a11ad6cf28dd
Panchajanya1999 <panchajanya@azure-dev.live>
2021-06-10 20:38:50 +0530

    msm: kgsl: Place worker thread on SCHED_RR
    On commit 254852b5b04c02668c9288676c9640bb1d46b2ec
    (msm: kgsl: Increase worker thread priority), we had increased
    the priority for better rendering frames.
    
    Since a lot of display related tasks are multiplexed on KGSL on a
    dedicated thread, they are placed in a FIFO manner, which is a bit
    of time consuming since not all those queued tasks have a known or
    small execution period.
    
    Switching to Round-Robin fashion eliminates this complexity and bounds
    these tasks on a time-slice and thus every tasks gets executed after
    a certain time, as fast as possible.
    
    Change-Id: I2b5137a5c6fdcc7fef7cafcc3e5728eab0034045
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5238,7 +5238,7 @@ static int __init kgsl_core_init(void)
 			"kgsl_low_prio_worker_thread")))
 		goto err;
 
-	sched_setscheduler(kgsl_driver.worker_thread, SCHED_FIFO, &param);
+	sched_setscheduler(kgsl_driver.worker_thread, SCHED_RR, &param);
 	/* kgsl_driver.low_prio_worker_thread should not be SCHED_FIFO */
 
 	kgsl_events_init();
