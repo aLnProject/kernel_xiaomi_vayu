de11cb500e41cc493efb43d3df5acec43480f44c
alanndz <alanndz7@gmail.com>
2022-08-03 15:01:19 +0000

    sched: features: Fix SCHEDTUNE_BOOST_UTIL
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -141,4 +141,4 @@
  * generally leads to usage of higher frequencies.
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
-#define SCHEDTUNE_BOOST_UTIL 0
+#define SCHED_FEAT_SCHEDTUNE_BOOST_UTIL 0
